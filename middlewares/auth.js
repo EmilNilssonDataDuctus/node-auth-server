a05de9356b82d5497660d52e01a5db504a7d2b97
Emil Nilsson <emil.nilsson@dataductus.se>
2023-06-13 15:53:19 +0200

    Add function isAuthenticated to middleware/auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -58,6 +58,65 @@ const generateAuthTokens = async (req, res, next) => {
   }
 };
 
+const isAuthenticated = async (req, res, next) => {
+  try {
+    // Attempt to find bearer token for authorization
+    // Unauthorized if missing
+    const authToken = req.get("Authorization");
+    const accessToken = authToken?.split("Bearer ")[1];
+    if (!accessToken) {
+      const error = createError.Unauthorized();
+      throw error;
+    }
+
+    // Attempt to access refreshToken from signedCookies
+    // Unauthorized if missing
+    const { signedCookies = {} } = req;
+    const { refreshToken } = signedCookies;
+    if (!refreshToken) {
+      const error = createError.Unauthorized();
+      throw error;
+    }
+
+    // Attempt to find same refreshToken in stored in database
+    // Unauthorized if missing
+    let refreshTokenInDB = tokens.find(
+      (token) => token.refreshToken === refreshToken
+    );
+    if (!refreshTokenInDB) {
+      const error = createError.Unauthorized();
+      throw error;
+    }
+    // Declare if found
+    refreshTokenInDB = refreshTokenInDB.refreshToken;
+
+    // Attempt to find decodedToken from jwt
+    // Unauthorized if missing
+    let decodedToken;
+    try {
+      decodedToken = jwt.verify(accessToken, ACCESS_TOKEN_SECRET);
+    } catch (err) {
+      const error = createError.Unauthorized();
+      return next(error);
+    }
+
+    // Attempt to find userId from jwt
+    // Unauthorized if missing
+    const { userId } = decodedToken;
+    const user = users.find((user) => user.id === userId);
+    if (!user) {
+      const error = createError.Unauthorized();
+      throw error;
+    }
+
+    req.userId = user.id;
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+};
+
 module.exports = {
   generateAuthTokens,
+  isAuthenticated,
 };
