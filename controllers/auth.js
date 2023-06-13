f60b9fd95d958dd4e57b3143657763648b95d1e9
Emil Nilsson <emil.nilsson@dataductus.se>
2023-06-13 14:33:56 +0200

    Add refreshAccessToken function to controllers/auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,7 @@ const createError = require("http-errors");
 
 const { clearTokens } = require("../utils/auth");
 
-const { users } = require("../data/data");
+const { users, tokens } = require("../data/data");
 
 const signUp = async (req, res, next) => {
   const { name, username, email, password } = req.body;
@@ -90,4 +90,58 @@ const logout = async (req, res, next) => {
   return res.sendStatus(204);
 };
 
-module.exports = { signUp, login, logout };
+const refreshAccessToken = async (req, res, next) => {
+  // Get env vars
+  const { REFRESH_TOKEN_SECRET, ACCESS_TOKEN_SECRET, ACCESS_TOKEN_LIFE } =
+    process.env;
+
+  // Access and validate refreshtoken from cookies
+  const { refreshToken: refreshTokenFromCookies } = req.signedCookies;
+  if (!refreshTokenFromCookies) {
+    return res.sendStatus(204);
+  }
+
+  try {
+    // Find and validate refreshtoken from database
+    const refreshTokenInDB = tokens.find(
+      (token) => token.refreshToken === refreshTokenFromCookies
+    )?.refreshToken;
+
+    if (!refreshTokenInDB) {
+      await clearTokens(req, res, next);
+      const error = createError.Unauthorized();
+      throw error;
+    }
+
+    try {
+      const decodedToken = jwt.verify(refreshToken, REFRESH_TOKEN_SECRET);
+      const { userId } = decodedToken;
+
+      const user = users.find((user) => user.id === userId);
+      if (!user) {
+        await clearTokens(req, res);
+        const error = createError("Invalid credentials", 401);
+        throw error;
+      }
+
+      // Generate JWT refreshtoken and send to user
+      const accessToken = generateJWT(
+        user.id,
+        ACCESS_TOKEN_SECRET,
+        ACCESS_TOKEN_LIFE
+      );
+
+      return res.status(200).json({
+        user,
+        accessToken,
+        expiresAt: new Date(Date.now() + ms(ACCESS_TOKEN_LIFE)),
+      });
+    } catch (error) {
+      return next(error);
+    }
+  } catch (error) {
+    return next(error);
+  }
+};
+
+module.exports = { signUp, login, logout, refreshAccessToken };
