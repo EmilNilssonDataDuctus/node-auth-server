40843bcc5c9c516a02fff3d90ec11f88812815f7
Emil Nilsson <emil.nilsson@dataductus.se>
2023-06-13 16:35:15 +0200

    Add util functions

---
+++ utils/auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+const { tokens } = require("../data/data");
+
+const dev = process.env.NODE_ENV === "development";
+
+// Wrapper function for JWT jsonwebtoken library
+const generateJWT = (userId, secret, expirationTime) => {
+  return jwt.sign({ userId }, secret, { expiresIn: expirationTime });
+};
+
+const clearTokens = async (req, res) => {
+  const { signedCookies = {} } = req;
+  const { refreshToken } = signedCookies;
+
+  if (refreshToken) {
+    const index = tokens.findIndex(
+      (token) => token.refreshToken === refreshToken
+    );
+    if (index) tokens.splice(index, 1);
+  }
+  res.clearTokens("refreshToken", {
+    httpOnly: true,
+    secure: !dev,
+    signed: true,
+  });
+};
+
+module.exports = { generateJWT, clearTokens };
